ac87212b4c8609eed1a6e0767ddb922cc79a3246
Nitesh Vyas <niteshvyas144@gmail.com>
2023-04-04 18:47:21 +0530

    Commit: - UIElements added

--- WalE/WalE/Features/PicOfTheDayFeature/LandingView/LandingViewController.swift
+++ WalE/WalE/Features/PicOfTheDayFeature/LandingView/LandingViewController.swift
@@ -16,6 +16,69 @@ class LandingViewController: UIViewController, LandingViewControllerProtocol {
     //MARK: - Properties
     var viewModel: LandingViewModelProtocol
     
+    //MARK:- UIElements
+    private lazy var containerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .clear
+        return view
+    }()
+    private let imageView: UIImageView = {
+        let imgView = UIImageView()
+        imgView.contentMode = .scaleAspectFill
+        return imgView
+    }()
+    
+    //Top section
+    private let lblCaption: UILabel = {
+        let lbl = UILabel()
+        lbl.numberOfLines = 0
+        lbl.textColor = .white
+        lbl.font = UIFont.boldSystemFont(ofSize: 28)
+        return lbl
+    }()
+    private let topContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .clear
+        view.layer.cornerRadius = 5.0
+        view.clipsToBounds = true
+        return view
+    }()
+    private let topBaseView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        view.alpha = 0.6
+        return view
+    }()
+    
+    // Bottom section
+    private let lblDescription: UILabel = {
+        let lbl = UILabel()
+        lbl.numberOfLines = 0
+        lbl.textColor = .white
+        lbl.font = UIFont.boldSystemFont(ofSize: 18.0)
+        return lbl
+    }()
+    private lazy var btnReadMore: UIButton = {
+        let btn = UIButton(type: .system)
+        btn.addTarget(self, action: #selector(self.showPicOfTheDayInFullScreen), for: .touchUpInside)
+        btn.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18.0)
+        btn.setTitle("View Picture of the day", for: .normal)
+        return btn
+    }()
+    private let bottomContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .clear
+        view.layer.cornerRadius = 5.0
+        view.clipsToBounds = true
+        return view
+    }()
+    private let bottomBaseView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        view.alpha = 0.6
+        return view
+    }()
+    
     //MARK: - Init Methods
     init(viewModel: LandingViewModelProtocol) {
         self.viewModel = viewModel
@@ -30,7 +93,18 @@ class LandingViewController: UIViewController, LandingViewControllerProtocol {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .yellow
+        setupUI()
         viewModel.getImageOfTheDay()
+    }
+    
+    //MARK:- Utility methods
+    func setupUI(){
+        self.navigationController?.isNavigationBarHidden = true
+    }
+    
+    //MARK: - Button Actions
+    @objc func showPicOfTheDayInFullScreen(){
+        print("\nShow full pic of the day - action")
     }
 }
 extension LandingViewController: LandingViewModelDelegateProtocol{
