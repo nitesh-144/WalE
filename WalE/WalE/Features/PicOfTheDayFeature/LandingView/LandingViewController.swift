b770938871935a62eb5edf262c692d05b2e357fa
Nitesh Vyas <niteshvyas144@gmail.com>
2023-04-04 17:07:47 +0530

    Commit: - Creating UI - Folder structure changed to manage features as component - Landing view files added

---
+++ WalE/WalE/Features/PicOfTheDayFeature/LandingView/LandingViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LandingViewController.swift
+//  WalE
+//
+//  Created by Deepika Vyas on 04/04/23.
+//
+
+import UIKit
+
+protocol LandingViewControllerProtocol{
+    
+}
+
+class LandingViewController: UIViewController, LandingViewControllerProtocol {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
---
+++ WalE/WalE/Features/PicOfTheDayFeature/LandingView/LandingViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  LandingViewModel.swift
+//  WalE
+//
+//  Created by Deepika Vyas on 04/04/23.
+//
+
+import Foundation
+
+protocol LandingViewModelProtocol{
+    
+}
+
+class LandingViewModel: LandingViewModelProtocol{
+    
+}
