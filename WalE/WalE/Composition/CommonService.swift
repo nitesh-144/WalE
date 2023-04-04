773f92c2686dabb1d2587d9e7fdef20407294bb6
Nitesh Vyas <niteshvyas144@gmail.com>
2023-04-04 18:36:43 +0530

    Commit: - Login added to optimize api calls, and to meet all use cases mentioned in the assignment

--- WalE/WalE/Composition/CommonService.swift
+++ WalE/WalE/Composition/CommonService.swift
@@ -12,7 +12,7 @@ protocol CommonServiceProtocol{
     var apiService: NetworkModuleProtocol { get set }
     var offlineStoreService: OfflineServiceProtocol { get set }
     
-    func getImageOfTheDay(imageData: @escaping (ImageDataModel) -> Void)
+    func getImageOfTheDay(imageRecord: @escaping (ImageDataModel) -> Void)
 }
 
 class CommonService: CommonServiceProtocol{
@@ -25,7 +25,40 @@ class CommonService: CommonServiceProtocol{
         self.offlineStoreService = dbStore
     }
     
-    func getImageOfTheDay(imageData: @escaping (ImageDataModel) -> Void) {
+    func getImageOfTheDay(imageRecord: @escaping (ImageDataModel) -> Void) {
         
+        let pastRecordDate = UserDefaultsManager.shared.getLastRecordSavedDate()
+        let pastRecordFlag = pastRecordDate != nil
+        let isToFetchFreshImageData = !pastRecordFlag || Utlity.checkTodayWithSavedRecordDate(savedRecordDate: pastRecordDate!)
+        
+        if isToFetchFreshImageData{
+            //Show loading
+            print("\nGetting from server\n")
+            apiService.getImageOfTheDay {[weak self] imageData, errorCases in
+                //Remove loading
+                switch errorCases{
+                case .internetConnectionError, .apiServiceError:
+                    if pastRecordFlag{
+                        //connection issue or something went wrong, use past day record
+                        //show alert and on click on that get Image data from DB
+                    }else{
+                        //nothing to show
+                        //show alert and on click on reload api
+                    }
+                case .none:
+                    imageRecord(imageData!)
+                    self?.offlineStoreService.clearDB()
+                    self?.offlineStoreService.saveImageDataToDB(imageModel: imageData!)
+                    UserDefaultsManager.shared.saveCurentDate()
+                }
+            }
+        }else{
+            print("\nGetting from DB\n\n")
+            if let offlineRecord = offlineStoreService.getImageData(){
+                imageRecord(offlineRecord)
+            }else{
+                //show alert - Not able to fetch data, on click - just Dismiss
+            }
+        }
     }
 }
